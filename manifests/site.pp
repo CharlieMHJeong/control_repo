d024581f65c865a63ec69ddcf51c0b0c3f582dbe
CharlieMHJEong <41171446+CharlieMHJeong@users.noreply.github.com>
2021-01-31 19:45:46 +1100

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -4,7 +4,7 @@ node 'master.puppet.vm' {
   include role::master_server
   file { '/root/README':
     ensure => file,
-    content => "Welcome to ${fqdn}\n", 
+    content => "\nWelcome to ${fqdn}\n", 
   }
 }
 
