7ae4beebfa1b0f7ac29770a5e6c578160a8dff14
CharlieMHJEong <41171446+CharlieMHJeong@users.noreply.github.com>
2021-02-01 13:40:03 +1100

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -4,7 +4,7 @@ class minecraft {
   }
   file { '/opt/minecraft/minecraft_server.jar':
     ensure => file,
-    source = 'https://launcher.mojang.com/v1/objects/1b557e7b033b583cd9f66746b7a9ab1ec1673ced/server.jar',
+    source => 'https://launcher.mojang.com/v1/objects/1b557e7b033b583cd9f66746b7a9ab1ec1673ced/server.jar',
   }
   package { 'java':
     ensure => present,
